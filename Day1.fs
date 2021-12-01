bf8e82e5d1212577f5103e3c41da9be8efe9bb35
Terence Hinrichsen <terence.hinrichsen@gmail.com>
2021-12-01 07:47:03 +0200

    Day 1 completed

--- Day1.fs
+++ Day1.fs
@@ -1,4 +1,32 @@
 module Day1
 
+  let sampleInput = [
+    199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263    ]
+
+  let input = TextFileReader.readFileContents "../../../Day1Input.txt"
   let run =
-    printfn "hello world!"
+
+    input
+    |> Seq.toList
+    |> List.pairwise
+    |> List.sumBy (fun (x, y) ->
+      if ( y|> int) > (x |> int) then 1 else 0 )
+    |> printfn "Answer for part 1 :%i"
+
+    input
+    |> Seq.toList
+    |> List.map int
+    |> List.windowed 3
+    |> List.map (List.sum)
+    |> List.pairwise
+    |> List.sumBy (fun (x, y) -> if ( y|> int) > (x |> int) then 1 else 0 )
+    |> printfn "Answer for part 2 :%i"
