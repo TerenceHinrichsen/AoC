0089eafbd64970f0d3781801f995fa4d26d234bb
Terence Hinrichsen <terence.hinrichsen@gmail.com>
2021-12-03 14:17:19 +0200

    Day3 part 2 completed - with the help of recursion. NOT elegant, but it works :)

--- Day3.fs
+++ Day3.fs
@@ -30,15 +30,15 @@ module Day3
       |> List.map (fun row ->
         row
         |> Seq.toList
-        |> List.mapi (fun i c -> i , (if c = '0' then 1 else 0) , (if c = '1' then 1 else 0)) )
+        |> List.mapi (fun i c -> i , c, (if c = '0' then 1 else 0) , (if c = '1' then 1 else 0)) )
       |> List.concat
 
     let gammaRate, epsilonRate =
         list
-        |> List.groupBy (fun (index,_,_) -> index)
+        |> List.groupBy (fun (index,_,_,_) -> index)
         |> List.map (fun (index, listOfInputs) ->
-          let numberOfZeros = listOfInputs |> List.sumBy (fun (_,a,b) -> a)
-          let numberOfOnes = listOfInputs |> List.sumBy (fun (_,_,b) -> b)
+          let numberOfZeros = listOfInputs |> List.sumBy (fun (_,_,a,b) -> a)
+          let numberOfOnes = listOfInputs |> List.sumBy (fun (_,_,_,b) -> b)
           if numberOfZeros > numberOfOnes then '0' else '1'
           , if numberOfZeros < numberOfOnes then '0' else '1' )
         |> List.unzip
@@ -49,4 +49,63 @@ module Day3
     printfn $"Gamma result {System.Convert.ToInt32(epsilonRate, 2)}"
 
     printfn $"Answer is {System.Convert.ToInt32(gammaRate, 2) * System.Convert.ToInt32(epsilonRate, 2)}"
-    ()
+
+    let list = (input |> List.ofSeq)
+
+    let findBitInString (inputL : List<string>) bitNumber =
+      inputL
+      |> List.map (fun st -> st.[bitNumber])
+      |> List.groupBy id
+      |> List.map (fun (char, charL) -> char, charL.Length)
+      |> List.sortBy fst
+      |> List.pairwise
+      |> List.head
+
+    let oxygenRating =
+      let rec filterDownL bitNumber (inputL : List<string>) =
+        match inputL with
+        | [x] -> x
+        | x ->
+          printfn $"Calculating bit {bitNumber}"
+          let zeros, ones = findBitInString x bitNumber
+          if ones |> snd >= (zeros |> snd) then
+            printfn $"Ones was greater {ones |> snd} vs {zeros |> snd }"
+            let newList =
+              x |> List.filter (fun y -> y.[bitNumber] = '1')
+            printfn $"New list is {newList} and contains {newList.Length} elements"
+            filterDownL (bitNumber + 1) newList
+          else
+            printfn $"Zeros was greater {ones |> snd} vs {zeros |> snd }"
+            let newList =
+              x |> List.filter (fun y -> y.[bitNumber] = '0')
+            printfn $"New list is {newList} and contains {newList.Length} elements"
+            filterDownL (bitNumber + 1) newList
+
+      filterDownL 0 list
+
+    printfn $"-------------- Oxygen rating is {oxygenRating} --------------------------"
+
+    let co2Rating =
+      let rec filterDownL bitNumber (inputL : List<string>) =
+        match inputL with
+        | [x] -> x
+        | x ->
+          printfn $"Calculating bit {bitNumber}"
+          let zeros, ones = findBitInString x bitNumber
+          if  (zeros |> snd) <= (ones |> snd) then
+            printfn $"Zeros was fewer {ones |> snd} vs {zeros |> snd }"
+            let newList =
+              x |> List.filter (fun y -> y.[bitNumber] = '0')
+            printfn $"New list is {newList} and contains {newList.Length} elements"
+            filterDownL (bitNumber + 1) newList
+          else
+            printfn $"Ones was fewer {ones |> snd} vs {zeros |> snd }"
+            let newList =
+              x |> List.filter (fun y -> y.[bitNumber] = '1')
+            printfn $"New list is {newList} and contains {newList.Length} elements"
+            filterDownL (bitNumber + 1) newList
+
+      filterDownL 0 list
+
+    printfn $"-------------- Oxygen rating is {co2Rating} --------------------------"
+    printfn $"Answer is {System.Convert.ToInt32(oxygenRating, 2) * System.Convert.ToInt32(co2Rating, 2)}"
